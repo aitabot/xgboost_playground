e8d8c20f12bc432df520a9fd6828c45921173352
zwarag <harrys.kavan@protonmail.com>
2021-04-27 20:24:24 +0200

    Ensure that no nan is possible

--- getDataFromAlphaVantage.py
+++ getDataFromAlphaVantage.py
@@ -25,7 +25,17 @@ def load_time_series(symbol: str, output_size="full") -> pd.DataFrame:
     return res
 
 
-def get_indicators(symbol: str, data=pd.DataFrame):
+def get_indicators(symbol: str, data: pd.DataFrame):
+    """Extends a DataFrame that contains prices with some technical indicators.
+
+    Args:
+        symbol (str): The Stock Symbol you're looking for.
+        data (pd.DataFrame): The DataFrame that is to be extended.
+
+    Returns:
+        data (pd.DataFrame): The extended DataFrame
+
+    """
     ti = TechIndicators(key=os.environ['KEY'], output_format='pandas')
     sma_time_period = 20
     ema_time_period = 21
@@ -34,7 +44,7 @@ def get_indicators(symbol: str, data=pd.DataFrame):
 
     sma, _ = ti.get_sma(symbol=symbol, time_period=sma_time_period)
     sma = sma.sort_values(['date'], ascending=[False])
-    data[f'SMA_{sma_time_period}'] = sma['SMA']
+    data[f'SMA_{sma_time_period}'] = sma
 
     ema, _ = ti.get_ema(symbol=symbol, time_period=ema_time_period)
     ema = ema.sort_values(['date'], ascending=[False])
@@ -43,9 +53,9 @@ def get_indicators(symbol: str, data=pd.DataFrame):
     # Positive when (the fast)SMA > (the slower)EMA
     # Generally it shows where support is in an up treand
     # or resistance when in an down trend
-    data[f'SMA_{sma_time_period}_gt_EMA{ema_time_period}'] = np.where(
-        data[f'SMA_{sma_time_period}'] >= data[f'EMA_{ema_time_period}'], 1.0, 0.0
-    )
+    # data[f'SMA_{sma_time_period}_gt_EMA{ema_time_period}'] = np.where(
+    #     data[f'SMA_{sma_time_period}'] >= data[f'EMA_{ema_time_period}'], 1.0, 0.0
+    # )
 
     rsi, _ = ti.get_rsi(symbol=symbol, time_period=rsi_time_period)
     rsi = rsi.sort_values(['date'], ascending=[False])
@@ -58,6 +68,14 @@ def get_indicators(symbol: str, data=pd.DataFrame):
     roc, _ = ti.get_roc(symbol=symbol, time_period=roc_time_period)
     roc = roc.sort_values(['date'], ascending=[False])
     data[f'ROC_{roc_time_period}'] = roc['ROC']
+
+    # TODO: refactor into a function
+    # I'm using two different ways of accesing the same data for no other reason then just trying it out.
+    data[f'SMA_{sma_time_period}'] = data[f'SMA_{sma_time_period}'].fillna(0)
+    data[f'EMA_{ema_time_period}'] = data[f'EMA_{ema_time_period}'].fillna(0)
+    data[f'RSI_{rsi_time_period}'] = data[f'RSI_{rsi_time_period}'].fillna(0)
+    data['OBV'] = obv['OBV'].fillna(0)
+    data[f'ROC_{roc_time_period}'] = data[f'ROC_{roc_time_period}'].fillna(0)
 
     return data
 
